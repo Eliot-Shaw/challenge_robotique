e548c0931e90f924a356055084eeca70cf5a2c23
eliot <eliot.shaw@etu.imt-nord-europe.fr>
2024-01-29 16:07:32 +0100

    pas cool clarice :-[

---
+++ code/chemin_base.py
@@ -0,0 +1,23 @@
+import math
+
+# cylindre type_cylindre :   coox      cooy      type_cylindre
+#                   1.7554    1.8713    3.0000
+ 
+# descrption des types_cylindre de cylindres gain, masse
+type_cylindre =[(1.0, 1.0),
+                (2.0, 2.0),
+                (3.0, 2.0)]
+
+def init_cylindres(donnees_map):
+    cylindres = [0.0,0.0,0.0] # creations des cylindres avec le bot en 0.0 une cylindre aussi
+    cylindres.append(donnees_map)
+    return cylindres 
+
+def calcul_dist(cylindre1, cylindre2):
+    distance_raw = math.sqrt((cylindre1[0] - cylindre2[0])**2 + (cylindre1[1] - cylindre2[1])**2)
+    distance_valeur = distance_raw/type_cylindre[cylindre2[2]-1][0]
+    distance_valeur_poids = distance_valeur*type_cylindre[cylindre2[2]-1][1]
+    return distance_valeur_poids
+
+def cylindre_plus_adapte(id_cylindre, cylindres):
+    
