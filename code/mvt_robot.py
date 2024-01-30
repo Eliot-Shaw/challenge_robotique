45e662e090469b88622178e6442906415602e1db
Clarice Goulet <kimi.goulet@gmail.com>
2024-01-30 14:34:08 +0100

    code

---
+++ code/mvt_robot.py
@@ -0,0 +1,14 @@
+import mcmc
+import math
+
+def go_point(point_x, point_y):
+    GO distance(robot, point)
+    STOP
+
+def tourner_point(point_x, point_y):
+    calcul_degré()
+    TURN angle(robot, point) # vérifier si tourne dans le bon sens en fonction du +/-, sinon inverser calcul degré
+
+def calcul_degré(orientation_robot):
+    orientation_to_point = 
+    return orientation_to_point - orientation_robot
