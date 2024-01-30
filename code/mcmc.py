6cf4da37d4c5d9be41e84ef685c28e52703fa57e
eliot <eliot.shaw@etu.imt-nord-europe.fr>
2024-01-30 14:18:16 +0100

    globalise mat

--- code/mcmc.py
+++ code/mcmc.py
@@ -70,12 +70,11 @@ def calculMatriceDis(lstV):
 
     n = len(lstV)
 
+    
     Mat = [ [ None for y in range( n ) ] for x in range( n ) ]
     for i in range(n):
         for j in range(n):
             Mat[i][j] = distancePercue(lstV[i], lstV[j])
-
-    return Mat
 
 def distanceAvecMat(VA, VB, lstV, matDis):
     iA = 0
@@ -166,7 +165,7 @@ def MCMC(N):
 def MCMC2(N, sigma1, a, b):
     Villes = np.concatenate((np.array([[0, 0, 0]]),importerVilles()), axis=0)
     m = len(Villes)
-    matDistance = calculMatriceDis(Villes)
+    matDistance = Mat
     sigma0 = sigma1
     lsigma0 = longueur(sigma0, Villes, matDistance)
     sigma = sigma0.copy()
@@ -236,24 +235,25 @@ def Tn(N, a, b, h = 1):
 
 sig = chem.faire_chemin()
 
+global() Mat
+Mat = calculMatriceDis(Villes)
 
 # l, sig0 = MCMC2(5000, sig, a=0, b=0) #iterations et chemin
-for i in range(10):
-    # Open a file in write mode
-    with open('../divers/resultats.txt', 'w') as f:
-        # Write the Python code to the file
-        for i in range(0,100,10):
-            a = 10
-            bz = 0.985+i*0.001
-            l, sig0 = MCMC2(5000, sig, a, bz) #iterations et chemin
-            f.write(f'a={a} --- b={bz} --- l={l}')
+# Open a file in write mode
+with open('../divers/resultats.txt', 'w') as f:
+    # Write the Python code to the file
+    for i in range(0,150,10):
+        a = 10
+        bz = 0.985+i*0.0001
+        l, sig0 = MCMC2(5000, sig, a, bz) #iterations et chemin
+        f.write(f'a={a} --- b={bz} --- l={l}\n')
 
 
 # l, sig = MCMC2(50000, sig0)
 
-print(sig) # chemin_base
-print(sig0) # mcmc 
-print(l) # longueur de mcmc
+# print(sig) # chemin_base
+# print(sig0) # mcmc 
+# print(l) # longueur de mcmc
 
 fig = plt.figure(1)
 
