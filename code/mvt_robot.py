457ae14cb11d58743d7d3cc50d58dd32cc299779
Clarice Goulet <kimi.goulet@gmail.com>
2024-01-30 14:54:25 +0100

    tourner

--- code/mvt_robot.py
+++ code/mvt_robot.py
@@ -5,13 +5,9 @@ def go_point(point_x, point_y):
     GO distance(robot, point)
     STOP
 
-def tourner_point(point_x, point_y):
-    calcul_degré()
-    TURN angle(robot, point) # vérifier si tourne dans le bon sens en fonction du +/-, sinon inverser calcul degré
-
-def calcul_degré(orientation_robot):
-    orientation_to_point = *
-    return orientation_to_point - orientation_robot
+def tourner_point(y_point, y_robot, x_point, x_robot):
+    angle = math.acos((y_point-y_robot)/math.sqrt((x_point-x_robot)**2+(y_point-y_robot)**2))
+    TURN angle # vérifier si tourne dans le bon sens en fonction du +/-, sinon inverser calcul degré
 
 
 def ecrire_plan(path_plan, directions):
