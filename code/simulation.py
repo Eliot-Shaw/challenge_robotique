333d97ffaf22a197faacadef1210b76ad9f60114
Eliot-Shaw <eliot.shaw@proton.me>
2024-01-31 12:04:35 +0100

    nett

--- code/simulation.py
+++ code/simulation.py
@@ -116,33 +116,8 @@ class Simu:
                 return
 
 
-
-        
-def recup_data_map():
-    argc = len(sys.argv)
-    if argc < 2:
-        print("preciser le nom du fichier de donnees en argument...")
-        exit()
-    #lecture du fichier
-    DataMap = open(sys.argv[1], 'r')
-    return DataMap.readlines()
-
 def main():
-    tutel = t.Turtle()
-    tutel.pd()
-    tutel.home()
-    instructions = recup_data_map()
-
-    for instruction in instructions:
-        sleep(0.01)
-        if(instruction[:4]) == "TURN":
-            tutel.left(float(instruction[5:]))
-            
-        
-        if(instruction[:2]) == "GO":
-            tutel.forward(float(instruction[2:])*5)
-
-    t.mainloop()
+    pass
 
 
 if __name__ == '__main__':
