8b4353d36e1c4360d0b3571168a7e6f1bdcf6dbd
Clarice Goulet <kimi.goulet@gmail.com>
2024-01-31 09:35:26 +0100

    feur tests

--- code/mvt_robot.py
+++ code/mvt_robot.py
@@ -13,12 +13,12 @@ def tourner_point(x_point, y_point, x_robot, y_robot):
     global theta_robot
     angle_tribute = math.atan2(y_point, x_point)
     print(f"angle_tribute : {angle_tribute}")
-    angle_point = math.atan2((y_point-y_robot),(x_point-x_robot))
+    angle_point = math.atan2((y_point),(x_point))
     print(f"angle point : {angle_point}")
     angle = angle_point - theta_robot
     angle = (angle)%(2*math.pi)-math.pi
     print(f"angle: {angle}")
-    theta_robot = angle_point
+    theta_robot += angle_point
     print(f"theta_robot : {theta_robot}")
     angle = angle*180/math.pi
     ajout_ordre_plan('TURN', angle) # vérifier si tourne dans le bon sens en fonction du +/-, sinon inverser calcul degré
