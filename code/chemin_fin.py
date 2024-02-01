b48b5340fcb56b5a754edb3f35f147babb7bf27f
Eliot-Shaw <eliot.shaw@proton.me>
2024-02-01 17:10:39 +0100

    testé

--- code/chemin_fin.py
+++ code/chemin_fin.py
@@ -12,17 +12,17 @@ type_cylindre =[(1.0, 1.0),
                 (2.0, 2.0),
                 (3.0, 2.0)]
 
-def calcul_dist(id1, id2):
+def calcul_dist(cylindres, id1, id2):
     distance_raw = math.sqrt((float(cylindres[id1][0]) - float(cylindres[id2][0]))**2 + (float(cylindres[id1][1]) - float(cylindres[id2][1]))**2)
     distance_valeur = distance_raw/type_cylindre[int(cylindres[id2][2])-1][0]
     distance_valeur_poids = distance_valeur*type_cylindre[int(cylindres[id2][2])-1][1]
     return distance_valeur_poids
 
-def choix_cylindre_suivant(id_cylindre):
+def choix_cylindre_suivant(cylindres, id_cylindre):
     distance_candidat = []
     for i in range(len(cylindres)):
         if i > id_cylindre:
-            distance_candidat.append(calcul_dist(id_cylindre, i))
+            distance_candidat.append(calcul_dist(cylindres, id_cylindre, i))
         else: 
             distance_candidat.append(999999)
     restultat = distance_candidat.index(min(distance_candidat))
@@ -37,5 +37,9 @@ def echanger_cylindres(cylindres, id_cylindre, id_voulue):
 
 def faire_chemin(cylindres, depth):
     for i in range(depth):
-        cylindres = echanger_cylindres(cylindres, i+1, choix_cylindre_suivant(i))
+        cylindres = echanger_cylindres(cylindres, i+1, choix_cylindre_suivant(cylindres, i))
     return cylindres
+
+map_cylindres = chemin_base.recup_data_map()
+print(map_cylindres[:4])
+print(faire_chemin(map_cylindres, 2)[:4])
