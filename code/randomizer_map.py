f5f95ad6fa5b381b97b98f9090840244e0a038ab
Eliot-Shaw <eliot.shaw@proton.me>
2024-02-01 14:31:24 +0100

    randomizer map propre

--- code/randomizer_map.py
+++ code/randomizer_map.py
@@ -1,12 +1,29 @@
-import math
 import random
+import math
+
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+def distance_minimale(x, y, points, seuil):
+    for point in points:
+        if distance(x, y, point[0], point[1]) < seuil:
+            return False
+    return True
 
 def ecrire_plan_txt(path_plan):
     with open(path_plan, 'w') as f:
-    # Write the Python code to the file
+        points = []
         for i in range(20):
-            f.write(f'{random.random()*25}\t{random.random()*25}\t{float(random.randint(1,3))}\n')
-
+            new_x = random.random() * 25
+            new_y = random.random() * 25
+            
+            # Vérifie si la distance minimale est respectée par rapport à tous les points précédents
+            while not distance_minimale(new_x, new_y, points, 0.85*2):
+                new_x = random.random() * 25
+                new_y = random.random() * 25
+            f.write(f'{new_x}\t{new_y}\t{float(random.randint(1,3))}\n')
+            points.append((new_x, new_y))
+            
 def main():
     ecrire_plan_txt('../divers/rng_donnees-map.txt')
     
